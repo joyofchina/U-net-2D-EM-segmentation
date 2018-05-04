2535403ec215b040d2ff8dd8a251a3d89d5a83c9
joyofchina <bhp0616@163.com>
2018-05-04 15:26:08 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from keras.preprocessing.image import img_to_array, load_img
+from keras.preprocessing.image import img_to_array, load_img, array_to_img
 import numpy as np 
 import glob
 #import cv2
